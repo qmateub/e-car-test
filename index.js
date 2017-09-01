9cb77134431cea44e8dfe2e43f91a280ada94efd
qmateub <qmateub@yahoo.es>
2017-09-01 13:05:03 +0200

    fix server

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-import { https } from "firebase-functions"
-import setupGraphQLServer from "./graphql/server"
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import mongoose from 'mongoose';
+import typeDefs from './graphql/data/schema';
+import resolvers from './sgraphqlc/data/resolvers';
 
-/* CF for Firebase with graphql-server-express */
-const graphQLServer = setupGraphQLServer()
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+// TODO username and password
+mongoose.connect('mongodb://guest:guest123@ds161833.mlab.com:61833/e_car');
 
-// https://us-central1-<project-name>.cloudfunctions.net/api
-export const api = https.onRequest(graphQLServer)
+const User = mongoose.model('User', {
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  createdAt: { type: Date, default: Date.now },
+  modifiedAt: { type: Date, default: Date.now },
+});
+
+const Block = mongoose.model('Block', {
+  name: String,
+  context: String,
+  tags: [String],
+  questions: [String],
+  createdAt: { type: Date, default: Date.now },
+  modifiedAt: { type: Date, default: Date.now },
+});
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({ schema, context: { User, Block } })
+);
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(PORT);
