194d55dcc3df520b7eb05bbe0bce0e0ad4e81dda
qmateub <qmateub@yahoo.es>
2017-09-07 11:51:27 +0200

    extract logical to separate files

--- index.js
+++ index.js
@@ -3,40 +3,20 @@ import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import mongoose from 'mongoose';
-import typeDefs from './graphql/data/schema';
-import resolvers from './graphql/data/resolvers';
+import typeDefs from './src/graphql/schema';
+import resolvers from './src/graphql/resolvers';
+import { User, Block } from './src/db/entities';
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
 
-// mongoose.connect('mongodb://guest:guest123@ds161833.mlab.com:61833/e_car');
 mongoose.connect(
   `mongodb://${process.env['MONGODB_USER']}:${process.env['MONGODB_PASS']}@${process.env[
     'MONGODB_URL'
   ]}`
 );
-
-const User = mongoose.model('User', {
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  createdAt: { type: Date, default: Date.now },
-  modifiedAt: { type: Date, default: Date.now },
-});
-
-const Block = mongoose.model('Block', {
-  name: String,
-  context: String,
-  tags: [String],
-  questions: [String],
-  createdAt: { type: Date, default: Date.now },
-  modifiedAt: { type: Date, default: Date.now },
-});
-
-const PORT = 3000;
 
 const app = express();
 
---
+++ src/db/entities.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+export const User = mongoose.model('User', {
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  createdAt: { type: Date, default: Date.now },
+  modifiedAt: { type: Date, default: Date.now },
+});
+
+export const Block = mongoose.model('Block', {
+  name: String,
+  context: String,
+  tags: [String],
+  questions: [String],
+  createdAt: { type: Date, default: Date.now },
+  modifiedAt: { type: Date, default: Date.now },
+});
